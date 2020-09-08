51a9631de9514d5996efb82955bd7d99fcf08905
Peter Ncube <pmn@outlook.com>
2020-09-08 16:01:39 +0100

    Create UserSysInfo.php

---
+++ UserSysInfo.php
@@ -0,0 +1,116 @@
+<?php
+
+/**
+ * Author: Peter Ncube
+ * Description: User Multi Factor athentication
+ *
+ */
+
+class SysInfo
+{
+    /**
+     * Get OS
+     */
+    public static function getOS()
+    {
+       $user_agent = $_SERVER['HTTP_USER_AGENT'];
+       $os_platform    = "Unknown OS Platform";
+       $os_array       = array('/windows phone 8/i'    =>  'Windows Phone 8',
+                               '/windows phone os 7/i' =>  'Windows Phone 7',
+                               '/windows nt 10.0/i'     =>  'Windows 10',
+                               '/windows nt 6.3/i'     =>  'Windows 8.1',
+                               '/windows nt 6.2/i'     =>  'Windows 8',
+                               '/windows nt 6.1/i'     =>  'Windows 7',
+                               '/windows nt 6.0/i'     =>  'Windows Vista',
+                               '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                               '/windows nt 5.1/i'     =>  'Windows XP',
+                               '/windows xp/i'         =>  'Windows XP',
+                               '/windows nt 5.0/i'     =>  'Windows 2000',
+                               '/windows me/i'         =>  'Windows ME',
+                               '/win98/i'              =>  'Windows 98',
+                               '/win95/i'              =>  'Windows 95',
+                               '/win16/i'              =>  'Windows 3.11',
+                               '/macintosh|mac os x/i' =>  'Mac OS X',
+                               '/mac_powerpc/i'        =>  'Mac OS 9',
+                               '/linux/i'              =>  'Linux',
+                               '/ubuntu/i'             =>  'Ubuntu',
+                               '/iphone/i'             =>  'iPhone',
+                               '/ipod/i'               =>  'iPod',
+                               '/ipad/i'               =>  'iPad',
+                               '/android/i'            =>  'Android',
+                               '/blackberry/i'         =>  'BlackBerry',
+                               '/webos/i'              =>  'Mobile'
+                            );
+       $found = false;
+       $device = '';
+       foreach ($os_array as $regex => $value) 
+       { 
+           if($found)
+            break;
+           else if (preg_match($regex, $user_agent)) 
+           {
+               $os_platform    =   $value;
+               $device = !preg_match('/(windows|mac|linux|ubuntu)/i',$os_platform)
+                         ?'MOBILE':(preg_match('/phone/i', $os_platform)?'MOBILE':'SYSTEM');
+           }
+       }
+       $device = !$device? 'SYSTEM':$device;
+       //return array('os'=>$os_platform,'device'=>$device);
+       return $os_platform;
+    }
+   
+    /**
+     * Get Browser
+     */
+    public static function getBrowser() 
+    {
+       $user_agent = $_SERVER['HTTP_USER_AGENT'];
+   
+       $browser        =   "Unknown Browser";
+   
+       $browser_array  = array('/msie/i'       =>  'Internet Explorer',
+                               '/firefox/i'    =>  'Firefox',
+                               '/safari/i'     =>  'Safari',
+                               '/chrome/i'     =>  'Chrome',
+                               '/opera/i'      =>  'Opera',
+                               '/netscape/i'   =>  'Netscape',
+                               '/maxthon/i'    =>  'Maxthon',
+                               '/konqueror/i'  =>  'Konqueror',
+                               '/mobile/i'     =>  'Handheld Browser'
+                            );
+   
+        $found = false;
+       foreach ($browser_array as $regex => $value) 
+       { 
+           if($found)
+            break;
+           else if (preg_match($regex, $user_agent,$result)) 
+           {
+               $browser    =   $value;
+           }
+       }
+       return $browser;
+    }
+
+    /**
+	 * Get IP
+	 */
+	public static function getIP()
+	{
+		if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+		{
+			$ip=$_SERVER['HTTP_CLIENT_IP'];
+		}
+		elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+		{
+			$ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+		}
+		else
+		{
+			$ip=$_SERVER['REMOTE_ADDR'];
+		}
+		return $ip;
+	}
+
+
+}
